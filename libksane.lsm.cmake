0f3d0bd36dce319c0e8c43c7fde99a3aae9c9646
Gilles Caulier <caulier.gilles@gmail.com>
2007-09-13 06:03:28 +0000

    extragear/libs from trunk (KDE4) : backport "libsanewidget" to new "libksane" component of extragear/libs. This new library will be used instead old libkscan to handle flat scanners supported by SANE library. libksane work better than libkscan and will be used by kipi-plugins and digiKam for KDE4. Major improvements will be add in the future like 16 bits color depth support...
    CCMAIL: kde-imaging@kde.org
    CCMAIL: kare.sars@kolumbus
    CCMAIL: digikam-devel@kde.org
    CCMAIL: boud@valdyas.org
    
    
    svn path=/trunk/extragear/libs/libksane/; revision=711921

---
+++ libksane.lsm.cmake
@@ -0,0 +1,15 @@
+Begin4
+Title: libkexiv2
+Version: ${KEXIV2_LIB_VERSION_STRING}
+Entered-date: 2007-05-08
+Description: An library to manipulate EXIF/IPTC information using Exiv2 library
+Keywords: kde image jpeg raw tiff png exif iptc digikam kipi-plugins
+Author: caulier dot gilles at gmail dot com (Gilles Caulier)
+ marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
+Maintained-by:
+Primary-site: http://www.kipi-plugins.org
+Alternate-site:
+Original-site:
+Platforms:
+Copying-policy: GPL
+End
