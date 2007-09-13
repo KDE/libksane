c580259d07ab75c64649eb300aba9f0c6ab6c5a2
Gilles Caulier <caulier.gilles@gmail.com>
2007-09-13 08:18:16 +0000

    fix header
    svn path=/trunk/extragear/libs/libksane/; revision=711991

--- libksane/sane_option.h
+++ libksane/sane_option.h
@@ -1,21 +1,24 @@
-/*
-   Copyright (C) 2007 Kåre Särs <kare.sars@kolumbus.fi>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2007-09-13
+ * Description : Sane interface for KDE
+ *
+ * Copyright (C) 2007 by Kare Sars <kare dot sars at kolumbus dot fi>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #ifndef SANE_OPTIONS_H
 #define SANE_OPTIONS_H
