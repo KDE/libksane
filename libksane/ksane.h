c0b9018f65ab7cab9b26afa50ec937284f58dbdc
Kåre Särs <kare.sars@iki.fi>
2008-01-02 17:54:11 +0000

    Add doxygen comments to ksane.h
    svn path=/trunk/extragear/libs/libksane/; revision=756075

--- libksane/ksane.h
+++ libksane/ksane.h
@@ -32,32 +32,67 @@
 
 #include "libksane_export.h"
 
+/** This namespace collects all methods and classes in LibKSane. */
 namespace KSaneIface
 {
 
 class KSaneWidgetPriv;
 
+/**
+ * This class provides the widget containing the scan options and the preview.
+ * @author Kare Sars <kare.sars@iki.fi>
+ */
 class LIBKSANE_EXPORT KSaneWidget : public QWidget
 {
     Q_OBJECT
 
 public:
+    /** This enumeration describes the type of the returned data.
+     * The number of formats might grow, so it is wise to be prepard fro more.*/
     typedef enum
     {
-        FormatBlackWhite,
-        FormatGrayScale8,
-        FormatGrayScale16,
-        FormatRGB_8_C,
-        FormatRGB_16_C,
-        FormatNone
+        FormatBlackWhite,   /**< One bit per pixel 1 = black 0 = white */
+        FormatGrayScale8,   /**< Grayscale with one byte per pixel 0 = black 255 = white */
+        FormatGrayScale16,  /**< Grayscale withtTwo bytes per pixel.
+                             * The byte order is the one provided by libsane. */
+        FormatRGB_8_C,      /**< Every pixel consists of three colors in the order Read,
+                             * Grean and Blue, with one byte per color (no alpha channel). */
+        FormatRGB_16_C,     /**< Every pixel consists of three colors in the order Read,
+                             * Grean and Blue, with two bytes per color(no alpha channel).
+                             * The byte order is the one provided by libsane. */
+        FormatNone = 0xFFFF /**< This enumeration value should never be returned to the user */
     } ImageFormat;
 
-
+    /** This constructor initializes the private class variables, but the widget is left empty.
+     * The options and the preview are added with the call to openDevice(). */
     KSaneWidget(QWidget* parent=0);
+
+    /** Standard destructor */
     ~KSaneWidget();
 
+    /** This helper method displays a dialog for selecting a scanner. The libsane
+     * device name of the selected scanner device is returned. */
     QString selectDevice(QWidget* parent=0);
+
+    /** This method opens the specified scanner device and adds the scan options and the
+     * preview window to the KSane QWidget.
+     * @param device_name is the libsane device name for the scanner to open.
+     * @return 'true' if all goes well and 'false' if the specified scanner can not be opened.
+     */
     bool    openDevice(const QString &device_name);
+
+    /**
+     * This is a convinience method that can be used to create a QImage from the image data
+     * returned by the imageReady(...) signal.
+     *
+     * @param data is the byte data containing the image.
+     * @param width is the width of the image in pixels.
+     * @param height is the height of the image in pixels.
+     * @param bytes_per_line is the number of bytes used per line. This might include padding
+     * and is probably only relevant for 'FormatBlackWhite'.
+     * @param format is the KSane image format of the data.
+     * @param img is the QImage reference to the image to fill.
+     */
     bool    makeQImage(const QByteArray &data,
                        int width,
                        int height,
@@ -65,19 +100,30 @@ public:
                        ImageFormat format,
                        QImage &img);
 
+    /** This method returns the vendor name of the scanner (Same as make). */
     QString vendor() const;
+    /** This method returns the make name of the scanner. */
     QString make() const;
+    /** This methos returns the model of the scanner. */
     QString model() const;
 
 
 public Q_SLOTS:
-
+    /** This method can be used to cancel a scan. */
     void scanCancel();
 
 Q_SIGNALS:
-
+    /**
+     * This Signal is emitted when a final scan is ready.
+     * @param data is the byte data containing the image.
+     * @param width is the width of the image in pixels.
+     * @param height is the height of the image in pixels.
+     * @param bytes_per_line is the number of bytes used per line. This might include padding
+     * and is probably only relevant for 'FormatBlackWhite'.
+     * @param format is the KSane image format of the data.
+     */
     void imageReady(QByteArray &data, int width, int height,
-                    int bytesperline, int format);
+                    int bytes_per_line, int format);
 
 private Q_SLOTS:
 
