d564cdd88a630a6355f2b6333e24cbdd2c31becb
Kåre Särs <kare.sars@iki.fi>
2008-01-15 07:15:17 +0000

    Change the license text GPL -> LGPL 2.1, 3, KDE e.V. +
    svn path=/trunk/extragear/libs/libksane/; revision=761578

--- libksane/labeled_entry.cpp
+++ libksane/labeled_entry.cpp
@@ -8,15 +8,21 @@
  *
  * Copyright (C) 2007 by Kare Sars <kare dot sars at kolumbus dot fi>
  *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * ============================================================ */
 
@@ -51,10 +57,10 @@ LabeledEntry::LabeledEntry(QWidget *parent, const QString& ltext)
     hb->addWidget(set);
     hb->activate();
 
-    connect(reset, SIGNAL(clicked()), 
+    connect(reset, SIGNAL(clicked()),
             this, SLOT(resetClicked()));
 
-    connect(set, SIGNAL(clicked()), 
+    connect(set, SIGNAL(clicked()),
             this, SLOT(setClicked()));
 }
 
