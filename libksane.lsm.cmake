18898c468ad101420a70894c027fdaba6d75440d
Gilles Caulier <caulier.gilles@gmail.com>
2008-03-18 18:45:47 +0000

    update
    svn path=/trunk/extragear/libs/libksane/; revision=787189

--- libksane.lsm.cmake
+++ libksane.lsm.cmake
@@ -2,13 +2,13 @@ Begin4
 Title: libksane
 Version: ${KSANE_LIB_VERSION_STRING}
 Entered-date: 2007-09-13
-Description: libksane is a KDE interface for SANE library to control flat scanners.
-Keywords: kde image sane scanner digikam kipi-plugins
+Description: libksane is a C++ library for SANE interface to control flat scanners.
+Keywords: kde image sane scanner digiKam kipi-plugins
 Author: caulier dot gilles at gmail dot com (Gilles Caulier)
  kare dot sars at iki dot fi (Kare Sars)
 Maintained-by:
 Primary-site: http://www.kipi-plugins.org
-Alternate-site:
+Alternate-site: http://www.digikam.org
 Original-site:
 Platforms:
 Copying-policy: LGPL
