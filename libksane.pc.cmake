b3ab52bb67abea98fa60490c3719b46bca31018c
Gilles Caulier <caulier.gilles@gmail.com>
2008-03-18 18:45:35 +0000

    update
    svn path=/trunk/extragear/libs/libksane/; revision=787188

--- libksane.pc.cmake
+++ libksane.pc.cmake
@@ -4,7 +4,8 @@ libdir=${LIB_INSTALL_DIR}
 includedir=${INCLUDE_INSTALL_DIR}
 
 Name: libksane
-Description: libksane is a KDE interface for SANE library to control flat scanners.
+Description: libksane is a C++ library for SANE interface to control flat scanners.
+URL: http://www.kipi-plugins.org
 Requires:
 Version: ${KSANE_LIB_VERSION_STRING}
 Libs: -L${LIB_INSTALL_DIR} -lksane
