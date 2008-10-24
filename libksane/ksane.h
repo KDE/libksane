2d1c3dc74df36cf14f14484b1585d5816addd180
Enrique Matías Sánchez <cronopios@gmail.com>
2008-10-24 07:10:18 +0000

    SVN_SILENT Fix typos in kdegraphics
    svn path=/trunk/KDE/kdegraphics/libs/libksane/; revision=875357

--- libksane/ksane.h
+++ libksane/ksane.h
@@ -139,20 +139,20 @@ public:
 
     /** This method can be used to write many parameter values at once.
      * @param opts is a QMap with the parameter names and values.
-     * @return This function returns the number of successfull writes.
+     * @return This function returns the number of successful writes.
      */
     int setOptVals(const QMap <QString, QString> &opts);
 
     /** This function reads one parameter value into a string.
      * @param optname is the name of the parameter to read.
      * @param value is the string representation of the value.
-     * @return this function returns true if the read was successfull.
+     * @return this function returns true if the read was successful.
      */
     bool getOptVal(const QString &optname, QString &value);
     /** This function writes one parameter value into a string.
      * @param optname is the name of the parameter to write.
      * @param value is the string representation of the value.
-     * @return this function returns true if the write was successfull.
+     * @return this function returns true if the write was successful.
      */
     bool setOptVal(const QString &optname, const QString &value);
 
