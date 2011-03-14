e36a7f9827f03b7984242ff5b4906ba6e520c1e2
Gilles Caulier <caulier.gilles@gmail.com>
2011-03-14 10:01:28 +0100

    use moc file

--- libksane/splittercollapser.cpp
+++ libksane/splittercollapser.cpp
@@ -22,7 +22,7 @@ License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 // Self
-#include "splittercollapser.h"
+#include "splittercollapser.moc"
 
 // Qt
 #include <QApplication>
