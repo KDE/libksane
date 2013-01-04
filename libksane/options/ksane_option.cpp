afd42b3b135465ab889cea8cb4e46929b95dee8b
Kåre Särs <kare.sars@iki.fi>
2013-01-04 23:41:19 +0200

    Fix byte order error on bigendian HW (cherry picked from commit b871f7ceeb53764c7191f1154cc4ec4f8ae9b9c7)

--- libksane/options/ksane_option.cpp
+++ libksane/options/ksane_option.cpp
@@ -162,19 +162,35 @@ void KSaneOption::readValue() {}
 SANE_Word KSaneOption::toSANE_Word(unsigned char *data)
 {
     SANE_Word tmp;
+    // if __BYTE_ORDER is not defined we get #if 0 == 0
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
     tmp  = (data[0]&0xff);
     tmp += ((SANE_Word)(data[1]&0xff))<<8;
     tmp += ((SANE_Word)(data[2]&0xff))<<16;
     tmp += ((SANE_Word)(data[3]&0xff))<<24;
+    #else
+    tmp  = (data[3]&0xff);
+    tmp += ((SANE_Word)(data[2]&0xff))<<8;
+    tmp += ((SANE_Word)(data[1]&0xff))<<16;
+    tmp += ((SANE_Word)(data[0]&0xff))<<24;
+    #endif
     return tmp;
 }
 
 void KSaneOption::fromSANE_Word(unsigned char *data, SANE_Word from)
 {
+    // if __BYTE_ORDER is not defined we get #if 0 == 0
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
     data[0] = (from & 0x000000FF);
     data[1] = (from & 0x0000FF00)>>8;
     data[2] = (from & 0x00FF0000)>>16;
     data[3] = (from & 0xFF000000)>>24;
+    #else
+    data[3] = (from & 0x000000FF);
+    data[2] = (from & 0x0000FF00)>>8;
+    data[1] = (from & 0x00FF0000)>>16;
+    data[0] = (from & 0xFF000000)>>24;
+    #endif
 }
 
 bool KSaneOption::getMinValue(float &) {return false;}
