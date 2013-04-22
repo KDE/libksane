679060e92d6b961fcf4b80fb6143e542ce5e5b83
Gilles Caulier <caulier.gilles@gmail.com>
2013-04-22 13:00:34 +0200

    fix CR SCAN #981401

--- libksane/options/ksane_opt_checkbox.cpp
+++ libksane/options/ksane_opt_checkbox.cpp
@@ -41,7 +41,7 @@ namespace KSaneIface
 {
 
 KSaneOptCheckBox::KSaneOptCheckBox(const SANE_Handle handle, const int index)
-: KSaneOption(handle, index), m_checkbox(0)
+: KSaneOption(handle, index), m_checkbox(0), m_checked(false)
 {
 }
 
