60d1ae254ff7df73a4d095489fddf10e36ee1b82
Nicolas Lécureuil <neoclust@mageia.org>
2015-08-24 09:26:25 +0200

    Fix headers location

--- libksane.pc.cmake
+++ libksane.pc.cmake
@@ -1,7 +1,7 @@
 prefix=${CMAKE_INSTALL_PREFIX}
 exec_prefix=${BIN_INSTALL_DIR}
 libdir=${LIB_INSTALL_DIR}
-includedir=${INCLUDE_INSTALL_DIR}
+includedir=${KF5_INCLUDE_INSTALL_DIR}
 
 Name: libksane
 Description: libksane is a C++ library for SANE interface to control flat scanners.
@@ -9,4 +9,4 @@ URL: http://www.digikam.org
 Requires:
 Version: ${KSANE_LIB_VERSION_STRING}
 Libs: -L${LIB_INSTALL_DIR} -lksane
-Cflags: -I${INCLUDE_INSTALL_DIR}
+Cflags: -I${KF5_INCLUDE_INSTALL_DIR}
